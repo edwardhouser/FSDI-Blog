86ab56afb78a9ff0e7156d2530efe738e51941c6
Edward Houser <edwardhouser@Ambers-MacBook-Air.local>
2025-05-31 00:55:56 -0700

    1255

--- config/settings.py
+++ config/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 STATICFILES_DIRS = [str(BASE_DIR.joinpath("static"))]
 STATIC_ROOT = str(BASE_DIR.joinpath("staticfiles"))
 STORAGES = {
